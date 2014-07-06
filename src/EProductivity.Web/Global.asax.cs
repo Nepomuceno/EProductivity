e4333e61d2e3f060696af294df5792bf17ce271c
bico <gabriel.bico@gmail.com>
2014-07-06 10:31:59 -0300

    Including Application insigts

--- src/EProductivity.Web/Global.asax.cs
+++ src/EProductivity.Web/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Microsoft.ApplicationInsights.Telemetry.Services;
 
 namespace EProductivity.Web
 {
@@ -16,6 +17,12 @@ namespace EProductivity.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ServerAnalytics.Start("beab1727-dedc-490b-91c2-1d9a5ffbb78d");
+        }
+        protected void Application_BeginRequest(object sender,EventArgs e)
+        {
+            ServerAnalytics.BeginRequest();
+            ServerAnalytics.CurrentRequest.LogEvent(Request.Url.AbsolutePath);
         }
     }
 }
